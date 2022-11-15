2e44e8915c1d0c48291be41c69f4498f6f854249
Stan Hustler <cf260469080@sina.com>
2022-11-15 15:29:42 +0800

    final edition

--- App.vue
+++ App.vue
@@ -2,6 +2,14 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			// 检测更新
+			// #ifdef APP-PLUS
+			this.$U.update()
+			// #endif
+			// 网络监听
+			this.$U.onNetWork()
+			// 初始化用户登录状态
+			this.$store.dispatch('initUser')
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -14,9 +22,25 @@
 
 <style>
 	/*每个页面公共css */
+	/* 官方css库 */
 	@import "./common/uni.css";
-	@import "./common/iconfont.css";
-	@import "./common/index.css";
+	/* 自定义图标库 */
+	@import "./common/icon.css";
+	/* 动画库 */
+	@import "./common/animate.css";
+	/* 自定义样式库 */
+	@import "./common/free.css";
+	/* 全局样式 */
 	@import "./common/common.css";
-	@import "animate.css";
+	
+	page{
+		background: #FFFFFF; 
+		height: 100%;
+	}
+	::-webkit-scrollbar{
+		display: none;
+	}
+	image{
+		background-color: #CCCCCC;
+	}
 </style>
